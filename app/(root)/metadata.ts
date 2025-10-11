e5679f5960cbfe047e4d2ae220977c0bda5a13eb
dennis gustavsson <dennisgustavsson87@gmail.com>
2025-10-11 11:33:54 +0200

    metadata fixup

--- app/(root)/metadata.ts
+++ app/(root)/metadata.ts
@@ -3,41 +3,43 @@ import type { Metadata } from "next";
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://kolbertatattoo.se";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(siteUrl),
-  title: "Kolberta Tattoo | Prisbelönt tatueringsstudio i Örebro",
-  description:
-    "Upplev skräddarsydda, prisbelönta tatueringar på Kolberta Tattoo i Örebro. Boka din tid med våra experttatuerare idag!",
-  keywords: [
-    "tattoo",
-    "Örebro",
-    "tatueringsstudio",
-    "prisbelönt",
-    "skräddarsydda tatueringar",
-    "Sverige",
-    "tatuerare",
-  ],
-  authors: [{ name: "Kolberta Tattoo", url: "https://kolbertatattoo.se" }],
-  openGraph: {
-    title: "Kolberta Tattoo | Prisbelönt tatueringsstudio i Örebro",
-    description:
-      "Upplev skräddarsydda, prisbelönta tatueringar på Kolberta Tattoo i Örebro. Boka din tid med våra experttatuerare idag!",
-    url: "https://kolbertatattoo.se",
-    siteName: "Kolberta Tattoo",
-    images: [
-      {
-        url: "/images/award-winning-tattoo-tatuerare-orebro-12.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Kolberta Tattoo Studio i Örebro",
-      },
-    ],
-    locale: "sv_SE",
-    type: "website",
-  },
+	metadataBase: new URL(siteUrl),
+	title: "Kolberta Tattoo | Prisbelönt tatuerare i Örebro",
+	description:
+		"Upplev skräddarsydda, prisbelönta tatueringar hos Kolberta Tattoo i Örebro. Boka din tid med Caroline idag!",
+	keywords: [
+		"tattoo",
+		"Örebro",
+		"tatueringsstudio",
+		"prisbelönt",
+		"skräddarsydda tatueringar",
+		"Sverige",
+		"tatuerare",
+		"neotraditional",
+		"kurbits",
+	],
+	authors: [{ name: "Kolberta Tattoo", url: "https://kolbertatattoo.se" }],
+	openGraph: {
+		title: "Kolberta Tattoo | Prisbelönt tatuerare i Örebro",
+		description:
+			"Upplev skräddarsydda, prisbelönta tatueringar hos Kolberta Tattoo i Örebro. Boka din tid med Caroline idag!",
+		url: "https://kolbertatattoo.se",
+		siteName: "Kolberta Tattoo",
+		images: [
+			{
+				url: "/images/award-winning-tattoo-tatuerare-orebro-12.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Kolberta Tattoo i Örebro",
+			},
+		],
+		locale: "sv_SE",
+		type: "website",
+	},
 
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-  },
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+	},
 };
